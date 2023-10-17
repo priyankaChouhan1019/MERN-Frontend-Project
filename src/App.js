cb4f193c32587fa7dbcb0df5247bd15d4e585975
priyankaChouhan1019 <priyachouhan1019@gmail.com>
2023-10-17 10:01:36 +0530

    mern front end page layouts

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route,Redirect,Switch} from 'react-router-dom'
+import Users from './users/components/pages/Users';
+import NewPlace from './places/components/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/components/pages/UserPlaces';
+
+
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation />
+        <main>
+      <Switch>
+      <Route path ='/' exact>
+        <Users/>
+      </Route>
+      <Route path= '/:userId/places' exact>
+        <UserPlaces/>
+      </Route>
+      <Route path ='/places/new' exact>
+        <NewPlace/>
+      </Route>
+      </Switch>
+      </main>
+     
+    </Router>
+  );
+}
+
+export default App;
